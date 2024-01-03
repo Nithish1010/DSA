c13d540bf55d65cddf03685bf8c236987924f968
Nithish babu G <83720516+Nithish1010@users.noreply.github.com>
2024-01-03 15:14:34 +0530

    Add files via upload

---
+++ BS.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class BS 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter how many elements to store: ");
+        int n=sc.nextInt();
+        int A[]=new int[10];
+        System.out.println("Enter elements to store: ");
+        for(int i=0;i<n;i++)
+        {
+            A[i]=sc.nextInt();
+        }
+        System.out.println("Enter elements to search: ");
+        int target=sc.nextInt();
+        int result=bs(A,target);
+        if(result!=-1)
+        {
+            System.out.println("\nElement "+target+" found at index"+result);
+        }
+        else
+        {
+            System.out.println("\nElement "+target+" not found");
+            System.out.println("\n"+result);
+        }
+    }
+    public static int bs(int[] A,int target)
+    {
+       int left=0;
+       int right=A.length-1;
+       while(left<=right)
+       {
+            int mid=(left+right)/2;
+            if(A[mid]==target)
+            {
+                return mid;
+            }
+            else if(A[mid]<target)
+            {
+                left=mid+1;
+            }
+            else
+            {
+                right=mid-1;
+            }
+       }
+       return-1;
+    }
+}
